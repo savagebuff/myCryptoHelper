28e477e29bcfbb7957144f0f0292aa55f7574482
choney <garaev-02@list.ru>
2022-01-29 02:14:38 +0300

    Info added on TableView

--- myCryptoHelper/CryptoTableViewCell.swift
+++ myCryptoHelper/CryptoTableViewCell.swift
@@ -7,8 +7,23 @@
 
 import UIKit
 
+struct CryptoTableViewCellViewModel {
+    let name: String
+    let symbol: String
+    let price: String
+}
+
 class CryptoTableViewCell: UITableViewCell {
 
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var symbolLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    
     static let identifier = "CryptoTableViewCell"
     
+    func configure(with viewModel: CryptoTableViewCellViewModel) {
+        nameLabel.text = viewModel.name
+        symbolLabel.text = viewModel.symbol
+        priceLabel.text = viewModel.price
+    }
 }
--- myCryptoHelper/CryptoViewController.swift
+++ myCryptoHelper/CryptoViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CryptoViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
+    
+    private var viewModels = [CryptoTableViewCellViewModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +19,18 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         
-        NomicsAPICaller.shared.getAllCryptoData { result in
+        NomicsAPICaller.shared.getAllCryptoData { [weak self] result in
             switch result {
             case .success(let models):
                 print(models.count)
                 print(models[0].name! + " - " + models[0].price!)
+                self?.viewModels = models.compactMap({
+                    CryptoTableViewCellViewModel(name: $0.name ?? "", symbol: $0.symbol ?? "", price: $0.price ?? "")
+                })
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
             case .failure(_):
                 print("no good(")
             }
@@ -30,16 +39,17 @@ class ViewController: UIViewController {
     
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension CryptoViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return viewModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CryptoTableViewCell.identifier, for: indexPath) as? CryptoTableViewCell else {
              fatalError()
         }
+        cell.configure(with: viewModels[indexPath.row])
         return cell
     }
 }
